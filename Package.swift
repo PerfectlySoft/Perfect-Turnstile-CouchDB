8d3c3e558082c052105985820e395a91d44d271e
Jonathan Guthrie <jono@perfect.org>
2016-12-05 10:05:18 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-11-26 22:23:57 +0000
+import PackageDescription
+let package = Package(
+    name: "PerfectTurnstileCouchDB",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/SwiftORM/CouchDB-StORM.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", versions: Version(1,0,0)..<Version(1,9223372036854775807,9223372036854775807)),
+    ]
+)
---
+++ Sources/SessionManager.swift
@@ -0,0 +1,56 @@
+//
+//  SessionManager.swift
+//  PerfectTurnstilePostgreSQL
+//
+//  Created by Jonathan Guthrie on 2016-10-19.
+//
+//
+
+import Foundation
+import TurnstileCrypto
+import Turnstile
+
+/**
+SQLiteSessionManager manages sessions via SQLite storage
+*/
+public class PerfectSessionManager: SessionManager {
+	/// Dictionary of sessions
+	//private var sessions = [String: String]()
+	private let random: Random = URandom()
+
+	/// Initializes the Session Manager. No config needed!
+	public init() {}
+
+	/// Creates a session for a given Subject object and returns the identifier.
+	public func createSession(account: Account) -> String {
+		let identifier = tokenStore?.new(account.uniqueID)
+		return identifier!
+	}
+
+	/// Deletes the session for a session identifier.
+	public func destroySession(identifier: String) {
+		let token = AccessTokenStore()
+		do {
+			try token.get(identifier)
+			try token.delete()
+		} catch {
+			print(error)
+		}
+	}
+
+	/**
+	Creates a Session-backed Account object from the Session store. This only
+	contains the SessionID.
+	*/
+	public func restoreAccount(fromSessionID identifier: String) throws -> Account {
+		let token = AccessTokenStore()
+		do {
+			try token.get(identifier)
+			guard token.check()! else { throw InvalidSessionError() }
+			return SessionAccount(uniqueID: token.userid)
+		} catch {
+			print("Error... \(error)")
+			throw InvalidSessionError()
+		}
+	}
+}
